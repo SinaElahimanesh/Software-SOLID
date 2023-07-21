09bfd442afbac328ac4a797d35f65a8879dbf2d8
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2023-07-21 19:23:47 +0330

    Add files via upload

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int computeArea() {
+        if (this.height < 0 || this.width < 0) {
+            return -1;
+        }
+        return this.height * this.width;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,54 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void computeAreaTest1() {
+        // Test 1
+        Rectangle rectangle1 = new Rectangle(0, 0);
+        assertEquals(0, rectangle1.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest2() {
+        // Test 2
+        Rectangle rectangle2 = new Rectangle(1, 10);
+        assertEquals(10, rectangle2.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest3() {
+        // Test 3
+        Rectangle rectangle3 = new Rectangle(3, 20);
+        assertEquals(60, rectangle3.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest4() {
+        // Test 4
+        Rectangle rectangle4 = new Rectangle(5, 5);
+        assertEquals(25, rectangle4.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest5() {
+        // Test 5
+        Rectangle rectangle5 = new Rectangle(-1, -3);
+        assertEquals(-1, rectangle5.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest6() {
+        // Test 6
+        Rectangle rectangle6 = new Rectangle(-1, 0);
+        assertEquals(-1, rectangle6.computeArea(), "Compute Area");
+    }
+
+    @Test
+    void computeAreaTest7() {
+        // Test 7
+        Rectangle rectangle7 = new Rectangle(-5, -3);
+        assertEquals(-1, rectangle7.computeArea(), "Compute Area");
+    }
+}
